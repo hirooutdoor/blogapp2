2fdd1d42b14861435272cb8c70539501375633b9
hiro <>
2020-12-05 06:10:24 +0900

    データベースの再作成 ・rollback ・user_idモデルを必須設定 ・seed.rbにユーザー情報追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do
-    Article.create(
+hiro = User.create!(email: 'hiro@hiro.com', password: 'password')
+riona = User.create!(email: 'riona@hiro.com', password: 'password')
+
+5.times do
+    hiro.articles.create!(
+        title: Faker::Lorem.sentence(word_count: 5),
+        content: Faker::Lorem.sentence(word_count: 100)
+    )
+end
+
+5.times do
+    riona.articles.create!(
         title: Faker::Lorem.sentence(word_count: 5),
         content: Faker::Lorem.sentence(word_count: 100)
     )
