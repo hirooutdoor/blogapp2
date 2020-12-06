830272e7fe1dd470139a2fa088aac724445e8f29
hiro <>
2020-12-07 06:26:10 +0900

    コメント一覧の表示です

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,20 @@ class CommentsController < ApplicationController
         article = Article.find(params[:article_id])
         @comment = article.comments.build
     end
+
+    def create
+        article = Article.find(params[:article_id])
+        @comment = article.comments.build(comment_params)
+        if @comment.save #render :newするときに＠（インスタンス変数）にしていないと使えない
+            redirect_to article_path(article), notice: '保存されました' #記事詳細ページに戻る
+        else
+            flash.now[:error] = '更新できませんでした'
+            render :new 
+        end
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
 end
