a62422d1ae90e15ed763467d8c060fb20e65b2d2
hiro <>
2020-12-21 15:34:26 +0900

    followingとfollowerの関係性を定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,13 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :favorite_articles, through: :likes, source: :article
   has_many :comments, dependent: :destroy
+
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   has_one :profile, dependent: :destroy
 
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
@@ -54,6 +61,10 @@ class User < ApplicationRecord
   # def gender
   #   profile&.gender
   # end
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
 
   def prepare_profile
     profile || build_profile
