e49b6ff33cb0509965e10802bfcca0748e8dff9e
hiro <>
2020-12-22 19:20:40 +0900

    jsで条件分岐していいねを表示/非表示にする →.hiddenクラスをつけて、それぞれ場合わけした時にそのhiddenクラスを取り除くメソッドをjs側で定義する！ ・scssでhiddenクラスにdisplay:none;定義してviewのいいねを非表示 ・viewの条件分岐を削除してhiddenクラスを追加、さらにactiveとinactiveをそれぞれ定義 ・jsで条件分岐で①hasLikedの場合.active-heartを取り除く②その他の場合.inactive-heartを取り除くことを記述

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,20 @@ require("channels")
 
 require("trix")
 require("@rails/actiontext")
+
+import $ from 'jquery'
+import axios from 'axios'
+
+document.addEventListener('turbolinks:load', () => {
+    const dataset = $('#article-show').data()
+    const articleId = dataset.articleId
+    axios.get(`/articles/${articleId}/like`)
+        .then((response) => {
+            const hasLiked = response.data.hasLiked
+            if (hasLiked) {
+                $('.active-heart').removeClass('hidden')
+            } else {
+                $('.inactive-heart').removeClass('hidden')
+            }
+        } )
+})
