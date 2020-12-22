92ed28e29698bd8fa0cd2a89b7f891cda0840e3d
hiro <>
2020-12-22 14:38:37 +0900

    自分がフォローしているユーザーの最新記事を取得する機能を実装 →showメソッドの定義

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,7 +1,12 @@
 class TimelinesController < ApplicationController
+    before_action :authenticate_user!
 
     def show
-        
+        #自分がフォローしている人のidを取得して記事を取得したい
+        user_ids = current_user.followings.pluck(:id)
+        #そのユーザーたちのidの記事を取得したい
+        @articles = Article.where(user_id: user_ids)
+        #user_idsに含まれているどれかのuser_idの記事を探してくるという意味、それを@articleとする
     end
 
 end
