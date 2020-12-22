c51024c52f472b3b5f2820cb30e29d122f21d66b
hiro <>
2020-12-22 21:56:31 +0900

    コメントをAjaxで追加できるようにした

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,12 +14,9 @@ class CommentsController < ApplicationController
     def create
         article = Article.find(params[:article_id])
         @comment = article.comments.build(comment_params)
-        if @comment.save #render :newするときに＠（インスタンス変数）にしていないと使えない
-            redirect_to article_path(article), notice: '保存されました' #記事詳細ページに戻る
-        else
-            flash.now[:error] = '更新できませんでした'
-            render :new 
-        end
+        @comment.save!
+
+        render json: @comment
     end
 
     private
