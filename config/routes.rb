5650c342f0dd5221b15fd2c21e3be02c49318e2d
hiro <>
2020-12-22 11:20:50 +0900

    Follow機能を表示されるようにする ・accounts_controllerの作成とshowメソッド定義 ・routesにてaccountsのresources作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root to: 'articles#index'
+
   resources :articles do
     resources :comments, only: [:new, :create]
+    
     resource :like, only: [:create, :destroy]
   end
+
+  resources :accounts, only: [:show]
 
   resource :profile, only: [:show, :edit, :update]
   resources :favorites, only: [:index]
