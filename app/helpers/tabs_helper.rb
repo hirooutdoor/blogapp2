135fb1791aae4fb8a6ed22d12dfe9c57b60dea6c
hiro <>
2020-12-24 21:12:24 +0900

    namespaceとscopeの設定

--- app/helpers/tabs_helper.rb
+++ app/helpers/tabs_helper.rb
@@ -1,6 +1,6 @@
 module TabsHelper
     def add_active_class(path)
-        path = path.split('?').first　#remove path after ?
+        path = path.split('?').first #remove path after ?
         'active' if current_page?(path)
     end
 end
